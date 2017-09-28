2fa2971a431ec9ec7ce0804c533230a53d412367
Ajani <pierre.lin@free.fr>
2017-09-29 00:51:42 +0200

    Add bittrex API wrapper

--- app/Business/BusinessBittrex.php
+++ app/Business/BusinessBittrex.php
@@ -58,6 +58,79 @@ class BusinessBittrex {
     }
 
     /**
+     * Place an order sell
+     * @param string $market : The market to sell on
+     * @param float $quantity : The quantity to sell
+     * @param float $rate : The rate to sell
+     * @param (optionnal) float $link_to_order : The uuid of the order to link to this one
+     * @return mixed : Order uuid if the order have been placed successfully, false if not
+     */
+    public function sell ($market, $quantity, $rate, $link_to_order = false)
+    {
+        $result = Bittrex::sellLimit($market, $quantity, $rate);
+
+        if ($result['success'] == false)
+        {
+            return false;
+        }
+
+        $transaction = new Transaction();
+        $transaction->strategy = $this->strategy;
+        $transaction->currencies = $market;
+        $transaction->quantity = $quantity;
+        $transaction->rate = $rate;
+        $transaction->fees = $this->compute_fees('buy', $quantity, $rate);
+        $transaction->status = 'open';
+        $transaction->type = 'sell';
+        $transaction->order_id = $result['result']['uuid'];
+        $transaction->remaining = $quantity;
+
+        if ($link_to_order)
+        {
+            $transaction->link_to_order = $link_to_order;
+        }
+
+        $transaction->save();
+
+        return $result['result']['uuid'];
+    }
+
+    /**
+     * Cancel an order
+     * @param string $order_id : The id of the order to cancel
+     * @return mixed : false if fail, true else
+     */
+    public function cancel ($order_id)
+    {
+        $result = Bittrex::cancelOrder($market, $quantity, $rate);
+
+        if ($result['success'] == false)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get an order
+     * @param string $order_id : The id of the order to get
+     * @return mixed : false if no order, the order else
+     */
+    public function get_order ($order_id)
+    {
+        $result = Bittrex::sellLimit($market, $quantity, $rate);
+
+        if ($result['success'] == false)
+        {
+            return false;
+        }
+
+        return $result['result'];
+    }
+
+
+    /**
      * Calcul fees
      * @param string $type : 'buy' or 'Sell'
      * @param float $quantity : Quantity to buy or sell
